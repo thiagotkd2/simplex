44fbcb77d6b620ae7ce906786ed6ea3c5a0030aa
thiagotkd2 <75231461+thiagotkd2@users.noreply.github.com>
2025-02-01 17:49:50 -0300

    Add files via upload

--- simplex.py
+++ simplex.py
@@ -9,7 +9,52 @@ def simplex(tipo_problema:int, numero_variaveis:int, numero_restricoes:int, func
             
     if(tipo_problema==-1):
         #min
-        print("asd")
+        f_obj_np=np.array(funcao_objetivo, dtype=float)
+        funcao_restricoes=np.array(restricoes, dtype=float)
+        for i in range(0,numero_variaveis):
+            matriz[0][i]=f_obj_np[i]
+        matriz[0][numero_variaveis::] = 0
+        print(funcao_restricoes)
+        for i in range(1,tamanho_linha):
+            for j in range(0, numero_variaveis):
+                matriz[i][j] = -funcao_restricoes[i-1][j]
+        
+        for i in range(1,tamanho_linha):
+            matriz[i][tamanho_coluna-1] = -funcao_restricoes[i-1][numero_variaveis]
+
+        for i in range(1,tamanho_linha):
+            matriz[i][numero_variaveis+i-1]=1
+
+        while(1):
+            coluna_pivot = np.argmin(matriz[0])
+
+            # linha_escolhida
+            # matriz[i][tamanho_coluna]/matriz[i][coluna_pivot]
+            razoes = np.zeros((tamanho_linha-1))
+            for i in range(1, tamanho_linha):
+                razoes[i-1]=matriz[i][tamanho_coluna-1]/matriz[i][coluna_pivot]
+                razoes[i-1]= float('inf') if razoes[i-1]<0 else razoes[i-1] # se for negativo, definir como inf
+
+
+            linha_pivot = np.argmin(razoes)+1
+            print(linha_pivot)
+            print(coluna_pivot)
+
+            matriz[linha_pivot] = matriz[linha_pivot]/matriz[linha_pivot][coluna_pivot] # linha_pivot/elemento_pivot
+            print(matriz)
+            linha_ja_calculada=np.zeros(tamanho_linha)
+            linha_ja_calculada[linha_pivot] = 1 # linha pivo já calculada
+            print(linha_ja_calculada)
+
+            # matriz[i] - (matriz[i][coluna_pivot])*matriz[linha_pivot]
+            for i in range(0,tamanho_linha):
+                if linha_ja_calculada[i]==0:
+                    matriz[i]=matriz[i] - (matriz[i][coluna_pivot])*matriz[linha_pivot]
+            print(matriz)
+            # for i in range(tamanho_linha):
+            #     for j in range(tamanho_coluna):
+            if(not np.any(matriz[0]<0)):
+                break
     else:
         #max
         f_obj_np=np.array(funcao_objetivo, dtype=float)*-1
@@ -27,13 +72,38 @@ def simplex(tipo_problema:int, numero_variaveis:int, numero_restricoes:int, func
 
         for i in range(1,tamanho_linha):
             matriz[i][numero_variaveis+i-1]=1
-        
+
+        while(1):
+            coluna_pivot = np.argmin(matriz[0])
+
+            # linha_escolhida
+            # matriz[i][tamanho_coluna]/matriz[i][coluna_pivot]
+            razoes = np.zeros((tamanho_linha-1))
+            for i in range(1, tamanho_linha):
+                razoes[i-1]=matriz[i][tamanho_coluna-1]/matriz[i][coluna_pivot]
+                razoes[i-1]= float('inf') if razoes[i-1]<0 else razoes[i-1] # se for negativo, definir como inf
 
 
-        print(matriz)
+            linha_pivot = np.argmin(razoes)+1
+            print(linha_pivot)
+            print(coluna_pivot)
 
-        # for i in range(tamanho_linha):
-        #     for j in range(tamanho_coluna):
+            matriz[linha_pivot] = matriz[linha_pivot]/matriz[linha_pivot][coluna_pivot] # linha_pivot/elemento_pivot
+            print(matriz)
+            linha_ja_calculada=np.zeros(tamanho_linha)
+            linha_ja_calculada[linha_pivot] = 1 # linha pivo já calculada
+            print(linha_ja_calculada)
+
+            # matriz[i] - (matriz[i][coluna_pivot])*matriz[linha_pivot]
+            for i in range(0,tamanho_linha):
+                if linha_ja_calculada[i]==0:
+                    matriz[i]=matriz[i] - (matriz[i][coluna_pivot])*matriz[linha_pivot]
+            print(matriz)
+            # for i in range(tamanho_linha):
+            #     for j in range(tamanho_coluna):
+            if(not np.any(matriz[0]<0)):
+                break
+    return matriz[0][tamanho_coluna-1]
         
         
 
@@ -78,4 +148,4 @@ lista_restricoes = [e.split() for e in restricoes]
 #print(func_obj_neg)
 print(tipo_problema)
 
-simplex(int(tipo_problema), int(numero_variaveis), int(numero_restricoes), funcao_objetivo, lista_restricoes)
+print(simplex(int(tipo_problema), int(numero_variaveis), int(numero_restricoes), funcao_objetivo, lista_restricoes))
